d1e903b4e8ac08844c1f708d3dbdb759854ef271
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-11-10 19:59:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-
+// stampare n numeri della sequenza di Fibonacci
 int main() {
-   return 0;
+    int PrimoNum=0, SecondoNum=0, ProssimoNum=0, n=0;
+    cin >>n;
+
+    if (n>=2){
+        PrimoNum=1;
+        SecondoNum=1;
+        cout << PrimoNum << endl << SecondoNum << endl;
+        for (int i = 2; i < n; i++) {
+            ProssimoNum=PrimoNum+SecondoNum;
+            PrimoNum= SecondoNum;
+            SecondoNum= ProssimoNum;
+            cout << ProssimoNum<< endl;
+        }
+    } else {
+        cout << "Errore" << endl;
+    }
+    return 0;
 }
